4f44777d68a15776fd6764c7a250d4a9ccd646e8
E-Wawrzynek <57082811+E-Wawrzynek@users.noreply.github.com>
2020-04-25 14:24:43 -0600

    some pseudocode

---
+++ playable_boards.py
@@ -0,0 +1,9 @@
+# take sukodu board
+# for easy, remove n = ? numbers
+# for medium, remove n = ? numbers
+# for hard, remove n = ? numbers
+
+# remove random cell, check:
+    # can it still be solved?
+    # if yes, continue removing
+    # if no, put number back in
